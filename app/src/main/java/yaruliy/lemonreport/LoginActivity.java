dd2a8df87fa2423db521874ab0ddb0ce87c05073
Yaroslav <l.yaruk1993@gmail.com>
2016-12-29 17:41:43 +0200

    first commit

---
+++ app/src/main/java/yaruliy/lemonreport/LoginActivity.java
@@ -0,0 +1,22 @@
+package yaruliy.lemonreport;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        final Button button = (Button) view;
+        final EditText login = (EditText) findViewById(R.id.editText);
+        final EditText email = (EditText) findViewById(R.id.editText3);
+
+        button.setText("asd");
+    }
+}
---
+++ app/src/main/java/yaruliy/lemonreport/SaveWorkActivity.java
@@ -0,0 +1,13 @@
+package yaruliy.lemonreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaveWorkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_work);
+    }
+}
